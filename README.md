c9406fad5d6fa972397734015d8d137f54f7852f
Christian Arlt <developer@chris.arlt.io>
2025-05-04 14:56:33 +0200

    * Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# evening-progressbar
+# Evening-Progressbar
 Arduino-powered progressbar for evening routine
+
+
+## Main idea
+This project enables an optical support for the evening routine (for the kids..). With the help of a RGB LED strip, this shows
+- (as first step) how far away from home daddy is
+- (for the other steps) how much time we already spent on this step (meaning, you get an idea, how much time is left)
+- and at which step we currently are
+
+To continue to the next step, we will use a button.
+
+The two progress bars are realized by a single LED strip that is cut in two pieces of a length you define based on the size of your deployment. Main point is, you connect only connect one of them to the Arduino.
+
+
+## Required hardware & other things
+- We used an Arduino Nano WiFi. Important is "WiFi", if you like to use the "how far away is daddy" feature
+- LED Strip. We used one of AZ Delivery on Amazon, which you can cut in smaller strips and wire manually
+- Something to put the LED strip and graphics of the steps on. We used frosted plexiglass, where you could put the LEDs on the back and pictograms for the steps on the front
+- A button. We used a buzzer, because BUZZER!
+
+For tracking the distance, you need an app on your phone along with a webserver for the php script.
+- As phone app, I'd recommend [GPSLogger](https://github.com/mendhak/gpslogger/) on Android, where you can enter an url with custom parameters and basic auth that matches this script here
+- As webserver, you may use a public one, as this one uses basic auth to prevent unauthorized access, BUT: note, that the Arduino requires plain http, which is unencrypted (vulnerable to man in the middle attacks).
+
+
+## How to use
+
+PHP:
+- Copy `credentials.json.example` (in the subdirectory) to `credentials.json` and insert your credentials and home coordinates
+- Upload all files to your webserver (make sure you include `.htaccess`!)
+
+Arduino:
+- In the first few lines, you need to configure all required parameters
---
+++ php/coordinate-share.php
@@ -0,0 +1,105 @@
+<?php
+
+// use external credentials
+$credentials = json_decode(file_get_contents('restricted/credentials.json'), true);
+
+
+/**
+ * Get distance between two GPS coordinates
+ * @param Object $coord1 First coordinate (latitude, longitude)
+ * @param Object $coord2 Second coordinate (latitude, longitude)
+ * @return float Distance in kilometers
+ */
+function haversine($coord1, $coord2)
+{
+    $earth_radius = 6371;
+    $lat1 = $coord1['latitude'];
+    $lon1 = $coord1['longitude'];
+    $lat2 = $coord2['latitude'];
+    $lon2 = $coord2['longitude'];
+
+    $delta_lat = deg2rad($lat2 - $lat1);
+    $delta_lon = deg2rad($lon2 - $lon1);
+
+    $a = sin($delta_lat / 2) * sin($delta_lat / 2) +
+         cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+         sin($delta_lon / 2) * sin($delta_lon / 2);
+
+    $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+    return $earth_radius * $c;
+}
+
+
+// POST-Request: Store GPS coordinate
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    if (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) ||
+        $_SERVER['PHP_AUTH_USER'] !== $credentials["post_user"]["username"] ||
+        $_SERVER['PHP_AUTH_PW'] !== $credentials["post_user"]["password"])
+    {
+            header('HTTP/1.0 401 Unauthorized');
+            echo "ERROR: Unauthorized";
+            exit;
+    }
+
+    if (isset($_GET['lat'], $_GET['lon']))
+    {
+        $gps_lat = floatval($_GET['lat']);
+        $gps_lon = floatval($_GET['lon']);
+       
+        $data = [
+            'latitude' => $gps_lat,
+            'longitude' => $gps_lon,
+            'timestamp' => time(),
+        ];
+
+        file_put_contents('restricted/coordinates.json', json_encode($data));
+
+        echo "Saved coordinates: " . $gps_lat . " , " . $gps_lon . " at " . date('Y-m-d H:i:s', $data['timestamp']);
+        exit;
+    }
+    else
+    {
+        echo "ERROR: No coordinates provided.";
+        exit;
+    }
+}
+
+// GET-Request: Get distance to reference point
+if ($_SERVER['REQUEST_METHOD'] === 'GET')
+{
+    if (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) ||
+        $_SERVER['PHP_AUTH_USER'] !== $credentials["get_user"]["username"] ||
+        $_SERVER['PHP_AUTH_PW'] !== $credentials["get_user"]["password"])
+    {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "ERROR: Unauthorized";
+        exit;
+    }
+
+    if (file_exists('restricted/coordinates.json'))
+    {
+        $data = json_decode(file_get_contents('restricted/coordinates.json'), true);
+        $current_time = time();
+        $time_diff = $current_time - $data['timestamp'];
+
+        // Only return distance if the data is not older than 2 hours (7200 seconds)
+        if ($time_diff > 7200)
+        {
+            echo "ERROR: Last update is too old.";
+            exit;
+        }
+        else
+        {
+            // Return distance in km
+            $distance = haversine($data, $credentials["target_coordinates"]);
+            echo number_format($distance, 2);
+            exit;
+        }
+    } else {
+        echo "ERROR: No saved coordinates.";
+        exit;
+    }
+}
+?>
